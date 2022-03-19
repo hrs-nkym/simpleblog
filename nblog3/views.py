30186b26527e2d03fa004fd8f869392dea84d2a5
leafhub <1803020+hrs-nkym@users.noreply.github.com>
2022-03-19 20:15:59 +0900

    fix: アプリのadmin追加, シリアライザー,ビュー追加

--- nblog3/views.py
+++ nblog3/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import Post, Category
+from .serializers import CategorySerializer, PostSerializer, SimplePostSerializer
+
+
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = SimplePostSerializer
+
+
+class PostDetail(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
