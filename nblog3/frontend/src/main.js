1b5c7067f17d4ab558035a9f07a7828cbcd764c1
leafhub <1803020+hrs-nkym@users.noreply.github.com>
2022-03-22 23:49:41 +0900

    fix: vue3 fetch detect.

--- nblog3/frontend/src/main.js
+++ nblog3/frontend/src/main.js
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App).use(store).use(router)
+
+app.config.globalProperties.$http = (url, opts) => fetch(url, opts)
+app.config.globalProperties.$httpPosts = 'http://127.0.0.1:8000/blog/api/posts/'
+app.config.globalProperties.$httpCategories = 'http://127.0.0.1:8000/blog/api/categories/'
+
+app.mount('#app')
